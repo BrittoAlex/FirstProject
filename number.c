a9ebed4021fb5fefc59b530d5816ebb5917dc4f7
Britto Alex <156065542+BrittoAlex@users.noreply.github.com>
2024-02-20 14:19:49 +0530

    Update number.c

--- number.c
+++ number.c
@@ -2,13 +2,13 @@
 
 int main() {    
 
-int number1,number2,sum;    
-printf("Enter two integers: ");
-scanf("%d %d",&number1,&number2);
+int number1,number2,number3,sum;    
+printf("Enter three integers: ");
+scanf("%d %d %d",&number1,&number2,&number3);
 
 // calculate the sum
-sum = number1 + number2;      
+sum = number1 + number2 + number3;      
 
-printf("%d + %d = %d", number1, number2, sum);
+printf("%d + %d = %d", number1, number2, number3, sum);
 return 0;
 }
